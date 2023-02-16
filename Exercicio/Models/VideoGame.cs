0e1f9903dfafcfc061dfdd1673422c2e5e40288e
heitorborbamarini <rm92976@fiap.com.br>
2023-02-16 10:35:54 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio/Models/VideoGame.cs
@@ -0,0 +1,27 @@
+﻿using Exercicio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Models
+{
+    internal class VideoGame: Console
+    {
+        public string Cor;
+        public string tipo;
+        public int jogo;
+        public bool ligado;
+
+        public void escolherJogp(int jogo)
+        {
+            jogo = novoJogo;
+        }
+
+        public void ligar(bool Estado)
+        {
+            ligado = Estado;
+        }
+    }
+}
---
+++ Exercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio.Models;
+
+Videogame videogame = new();
+
+videogame.cor = "preto";
+videogame.tipo = "console";
+
+Console.WriteLine("Ligar videogame? (s/n)")
+string funcionamento Readline();
+if (estado == "S")
+{
+    videogame.ligado = true;
+    videogame.ligar = (videogame.ligado);
+} else { videogame.ligado = false}
+
+if (videogame.ligado)
+{
+    Console.WriteLine("escolha o jogo: "):
+    int jogo =Convert.Int32( Console.ReadLine() );
+    if (jogo < 0) throw new Exception("jogo indisponivel");
+
+    videogame.jogo = jogo;
+
+    videogame.escolherJogo(jogo);
+}
